718ca8e694efd9708ecbd1e85e14a7b7b9dbaeea
MatMatHelou <2396157@cmontmorency.qc.ca>
2025-10-16 22:54:10 -0400

    finalisation3

--- Projet/node_2d.gd
+++ Projet/node_2d.gd
@@ -7,16 +7,24 @@ var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var direction = 30
 
+@onready var sprite_2d: = $chien/Sprite2D
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
+	# Animations
+	if velocity.x > 1 or velocity.x < -1:
+		sprite_2d.animation = "walk"
+	else:
+		sprite_2d.animation = "idle"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		sprite_2d.animation = "walk"
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_power * jump_multiplier
@@ -30,3 +38,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
 	move_and_slide()
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
